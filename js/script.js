0bfac60b92b740bf33c554cc2965f77794e9499a
Timofey <tim02timka@mail.ru>
2021-10-11 21:33:16 +0700

    anim

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
 
     // Анимация при скролл 
+    const startAnim = document.querySelectorAll('.startAnim'),
+          startBlock = document.querySelectorAll('.startBlock');
 
+    console.dir(startBlock[0]);
+    
+    function showElem(elem){
+        if (window.scrollY > elem.offsetTop + 100){
+            
+            let startAnimChild = elem.querySelectorAll('.startAnim');
+
+            startAnimChild.forEach(item =>{
+                item.classList.add('endAnim');
+            })
+        }
+    }
+    window.addEventListener('scroll', showElem(startBlock[0]) )
+   
 })
